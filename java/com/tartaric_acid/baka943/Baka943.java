559235cacec25ea72c573b15a0836699f248dcc8
TartaricAcid <568248266@qq.com>
2016-11-26 12:55:04 +0800

    fix some error

--- java/com/tartaric_acid/baka943/Baka943.java
+++ java/com/tartaric_acid/baka943/Baka943.java
@@ -10,12 +10,20 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Baka943.MODID, name = Baka943.NAME, version = Baka943.VERSION, acceptedMinecraftVersions = "[1.9,)")
+@Mod(modid = Baka943.MODID, 
+     name = Baka943.NAME, 
+     version = Baka943.VERSION, 
+     acceptedMinecraftVersions = Baka943.AMVersions, 
+     dependencies = Baka943.DEPENDENCIES
+     )
+
 public class Baka943
 {
     public static final String MODID = "baka943";
     public static final String NAME = "Baka943";
     public static final String VERSION = "1.0.3";
+    public static final String AMVersions = "[1.9, 1.10.2]";
+    public static final String DEPENDENCIES ="required-after:Botania@[r1.9-332,)";
 
     @Instance(Baka943.MODID)
     public static Baka943 instance;
